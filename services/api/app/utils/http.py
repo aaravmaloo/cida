30ed3f24429122046a2a7262e5f0e448fd4b8475
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-15 14:26:54 +0530

    create http.py

---
+++ services/api/app/utils/http.py
@@ -0,0 +1,11 @@
+from fastapi import Request
+
+
+def client_ip(request: Request) -> str:
+    forwarded_for = request.headers.get("x-forwarded-for")
+    if forwarded_for:
+        return forwarded_for.split(",")[0].strip()
+    if request.client:
+        return request.client.host
+    return "unknown"
+
