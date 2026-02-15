5c7f7b127e2d555b4870213c1f17da03980acdb4
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-15 14:48:20 +0530

    chore(api): initialize models module

---
+++ services/api/app/models/__init__.py
@@ -0,0 +1,18 @@
+from app.models.entities import (
+    AdminAuditLog,
+    AnalysisEvent,
+    HumanizeEvent,
+    ModelRegistry,
+    ReportJob,
+    UsageBucket,
+)
+
+__all__ = [
+    "AnalysisEvent",
+    "HumanizeEvent",
+    "ReportJob",
+    "UsageBucket",
+    "AdminAuditLog",
+    "ModelRegistry",
+]
+
